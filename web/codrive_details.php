81dea75e835dd1d51eb04d2d7714bb50d5b733b6
Esteban Garcia <Ayrathe@gmail.com>
2025-07-23 01:19:30 +0200

    Mise à jour de la page codrive_details.php

--- web/codrive_details.php
+++ web/codrive_details.php
@@ -7,7 +7,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet" />
-  <link href="stylesheet/style.css" rel="stylesheet" />
+  <link href="stylesheet/codrive_details.css" rel="stylesheet" />
 </head>
 <body>
 
